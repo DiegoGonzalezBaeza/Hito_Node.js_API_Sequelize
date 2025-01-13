7c4e476b18cbed0461bdec3c68c2fb793d6dd7c9
DiegoGonzalezBaeza <diego.gonzalez.baeza@gmail.com>
2025-01-13 06:04:14 -0300

    update

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import pg from "pg";
 
 const {Pool} = pg;
@@ -5,7 +6,7 @@ const {Pool} = pg;
 // const connectionString = 'postgresql://dbuser:secretpassword@database.server.com:3211/mydb'
 // cambiar con los datos de docker-compose.yml:
 
-const connectionString = 'postgresql://postgres:root@localhost:5434/db_hito'
+const connectionString = process.env.CONNECT_DB;
 
 export const pool = new Pool({
     connectionString,
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import userRoute from "./routes/user.route";
 import authRoute from "./routes/auth.route";
